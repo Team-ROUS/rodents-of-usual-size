9964a2810108ed0cef04cde44ff16092492b5088
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-07-24 11:12:32 +1200

    change the functino to laser_update

--- Laser.gd
+++ Laser.gd
@@ -6,22 +6,6 @@ var PORT
 
 onready var com = $Com
 
-func plugin_installed():
-	var directory = Directory.new();
-	return directory.file_exists("res://addons/GDSerCommDock/bin/libGDSercomm.dylib")
-
-func _ready():
-	if plugin_installed():
-		PORT = SERCOMM.new()
-		PORT.open(
-			"/dev/cu.usbserial-120",
-			9600,
-			1000,
-			com.bytesz.SER_BYTESZ_8, 
-			com.parity.SER_PAR_NONE, 
-			com.stopbyte.SER_STOPB_ONE)
-		PORT.flush()
-
 var maps = [
 	"6,0,RRRRRRRRRRRUUURRRR",
 	"0,2,RRRRRUUUUURRRRRUUURRR",
@@ -30,14 +14,36 @@ var maps = [
 	"0,0,RUURDRRUUUULUUURRDRUULLLLLUUURRDRUUURDDDDRRDDDRDDDDRURDDRUURUUUURUUULLLDLDRRDLLLUUUUURRRRDRR",
 ]
 var map_idx = 0
+var player_progress = 0
+var last_change = OS.get_ticks_msec()
+
+func plugin_installed():
+	var directory = Directory.new();
+	return directory.file_exists("res://addons/GDSerCommDock/bin/libGDSercomm.dylib")
+
+func _ready():
+	if plugin_installed():
+		PORT = SERCOMM.new()
+		PORT.open(
+			"/dev/cu.usbserial-0001",
+			9600,
+			1000,
+			com.bytesz.SER_BYTESZ_8, 
+			com.parity.SER_PAR_NONE, 
+			com.stopbyte.SER_STOPB_ONE)
+		PORT.flush()
+		laser_update()
 
 func laser_set_map():
 	if plugin_installed():
-		PORT.write(maps[map_idx])
+		var tmp = "%d,%d\n" % [map_idx, player_progress]
+		PORT.write(tmp)
 		PORT.flush()
 
-func laser_change_map():
-	map_idx += 1
-	if map_idx >= len(maps):
-		map_idx = 0
-	laser_set_map()
+func laser_update():
+	if OS.get_ticks_msec() - last_change > 150:
+		player_progress += 1
+		if player_progress >= len(maps[map_idx]):
+			player_progress = 0
+		laser_set_map()
+		last_change = OS.get_ticks_msec()
--- Player.gd
+++ Player.gd
@@ -75,7 +75,7 @@ func _physics_process(delta):
 
 
 	if Input.get_action_strength("ui_accept") > 0:
-		$Laser.laser_change_map()
+		$Laser.laser_update()
 
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
