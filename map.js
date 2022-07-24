029ad6f757a5215414aacd7eb7146b0ed1932f71
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-07-24 12:22:28 +1200

    refactor to get_converters func

--- map.js
+++ map.js
@@ -28,50 +28,38 @@ var mapping_6 = [
     [707.144226, 318.925262],
 ];
 
-mapping_1[1][1] *= -1;
-mapping_6[1][1] *= -1;
+function get_converters(mapping_1, mapping_6) {
+    mapping_1[1][1] *= -1;
+    mapping_6[1][1] *= -1;
 
-console.log(mapping_1);
-console.log(mapping_6);
+    var dx_excel = mapping_6[0][0] - mapping_1[0][0]
+    var dx_godot = mapping_6[1][0] - mapping_1[1][0]
 
-var dx_excel = mapping_6[0][0] - mapping_1[0][0]
-var dx_godot = mapping_6[1][0] - mapping_1[1][0]
+    var dy_excel = mapping_6[0][1] - mapping_1[0][1]
+    var dy_godot = mapping_6[1][1] - mapping_1[1][1]
 
-var dy_excel = mapping_6[0][1] - mapping_1[0][1]
-var dy_godot = mapping_6[1][1] - mapping_1[1][1]
+    var godot_x_multiplier = dx_godot/dx_excel
+    var godot_y_multiplier = dy_godot/dy_excel
 
-console.log(dx_excel)
-console.log(dx_godot)
+    var godot_x1_simulated = godot_x_multiplier * mapping_1[0][0]
+    var godot_y1_simulated = godot_y_multiplier * mapping_1[0][1]
 
-console.log(dy_excel)
-console.log(dy_godot)
+    var godot_x1_diversion = godot_x1_simulated - mapping_1[1][0]
+    var godot_y1_diversion = godot_y1_simulated - mapping_1[1][1]
 
-var godot_x_multiplier = dx_godot/dx_excel
-var godot_y_multiplier = dy_godot/dy_excel
-
-console.log(godot_x_multiplier)
-console.log(godot_y_multiplier)
-
-var godot_x1_simulated = godot_x_multiplier * mapping_1[0][0]
-var godot_y1_simulated = godot_y_multiplier * mapping_1[0][1]
-
-console.log(godot_x1_simulated)
-console.log(godot_y1_simulated)
-
-var godot_x1_diversion = godot_x1_simulated - mapping_1[1][0]
-var godot_y1_diversion = godot_y1_simulated - mapping_1[1][1]
-
-console.log(godot_x1_diversion)
-console.log(godot_y1_diversion)
-
-function formula_translate_x_from_excel(x) {
-    return godot_x_multiplier * x - godot_x1_diversion
-}
-function formula_translate_y_from_excel(y) {
-    return godot_y_multiplier * y - godot_y1_diversion
+    function formula_translate_x_from_excel(x) {
+        return godot_x_multiplier * x - godot_x1_diversion
+    }
+    function formula_translate_y_from_excel(y) {
+        return godot_y_multiplier * y - godot_y1_diversion
+    }
+    return {
+        formula_translate_x_from_excel,
+        formula_translate_y_from_excel
+    }
 }
 
-console.log(mapping_1[0][0], formula_translate_x_from_excel(mapping_1[0][0]), mapping_1[1][0])
-console.log(mapping_1[0][1], formula_translate_y_from_excel(mapping_1[0][1]), mapping_1[1][1])
-
+const converters = get_converters(mapping_1, mapping_6)
+console.log(mapping_1[0][0], converters.formula_translate_x_from_excel(mapping_1[0][0]), mapping_1[1][0])
+console.log(mapping_1[0][1], converters.formula_translate_y_from_excel(mapping_1[0][1]), mapping_1[1][1])
 
